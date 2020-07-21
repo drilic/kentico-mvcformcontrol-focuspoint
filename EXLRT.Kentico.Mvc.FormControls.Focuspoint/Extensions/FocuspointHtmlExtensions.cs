efb0a9fa19d6299c458a1511f097ebc0417e232a
Dragoljub Ilic <dragoljub.ilic@exlrt.com>
2020-07-21 11:46:16 +0200

    Initial commit

---
+++ EXLRT.Kentico.Mvc.FormControls.Focuspoint/Extensions/FocuspointHtmlExtensions.cs
@@ -0,0 +1,137 @@
+﻿using CMS.Helpers;
+using Kentico.Content.Web.Mvc;
+using Kentico.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web.Mvc;
+
+namespace EXLRT.Kentico.Mvc.FormControls.Focuspoint.Extensions
+{
+    public static class FocuspointHtmlExtensions
+    {
+        public static MvcHtmlString RenderFocuspointStyles(this HtmlHelper htmlHelper, string path = "~/Content/FormControls/Focuspoint/css/focuspoint.css")
+        {
+            UrlHelper urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+
+            return MvcHtmlString.Create($"<link href='{urlHelper.Content(path)}' rel='stylesheet' type='text/css' />");
+        }
+
+        public static MvcHtmlString RenderFocuspointSripts(this HtmlHelper htmlHelper, string path = "~/Content/FormControls/Focuspoint/js/jquery.focuspoint.js",
+            int throttleDuration = 17, bool reCalcOnWindowResize = true)
+        {
+            UrlHelper urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+
+            StringBuilder script = new StringBuilder();
+            script.Append($"<script src='{urlHelper.Content(path)}'></script>");
+            script.Append($@"<script type='text/javascript'>
+                                (function ($) {{
+                                    $(document).ready(function () {{
+                                        $('.js-focuspoint').focusPoint({{
+                                            throttleDuration: {throttleDuration},
+                                            reCalcOnWindowResize: {reCalcOnWindowResize.ToString().ToLowerInvariant()}
+                                        }});
+                                    }});
+                                }}(jQuery));
+                            </script>");
+
+            return MvcHtmlString.Create(script.ToString());
+        }
+
+        public static MvcHtmlString FocuspointImage(this HtmlHelper htmlHelper, string imageUrl,
+            string cssContainerClass = "",
+            string imageAlt = "",
+            string imageTitle = "",
+            string cssImageClass = "",
+            object htmlImageAttributes = null)
+        {
+            if (string.IsNullOrEmpty(imageUrl))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            string focusX = string.Empty;
+            string focusY = string.Empty;
+            string[] separateURL = imageUrl.Split('?');
+
+            UrlHelper urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+            TagBuilder imageElement = new TagBuilder("img");
+
+            if (separateURL.Length == 2)
+            {
+                NameValueCollection queryString = System.Web.HttpUtility.ParseQueryString(separateURL[1]);
+
+                focusX = ValidationHelper.GetString(queryString.Get("focus-x"), string.Empty);
+                focusY = ValidationHelper.GetString(queryString.Get("focus-y"), string.Empty);
+                queryString.Remove("focus-x");
+                queryString.Remove("focus-y");
+
+                string cleanUrl = separateURL[0];
+                if (queryString.Count > 0)
+                {
+                    cleanUrl = $"{cleanUrl}?{queryString.ToString()}";
+                }
+
+                imageElement.SafeMergeAttribute("src", urlHelper.Kentico().ImageUrl(cleanUrl, SizeConstraint.Empty));
+                imageElement.SafeMergeAttribute("alt", imageAlt);
+                imageElement.SafeMergeAttribute("title", imageTitle);
+
+                if (!String.IsNullOrEmpty(cssImageClass))
+                {
+                    imageElement.AddCssClass(cssImageClass);
+                }
+
+                if (htmlImageAttributes != null)
+                {
+                    Dictionary<string, object> additionalAttributes = new Dictionary<string, object>();
+                    foreach (PropertyInfo attr in htmlImageAttributes.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                    {
+                        additionalAttributes.AddSafe(attr.Name.Replace("_", "-"), attr.GetValue(htmlImageAttributes, null));
+                    }
+
+                    if (additionalAttributes.Any())
+                    {
+                        imageElement.MergeAttributes(additionalAttributes);
+                    }
+                }
+            }
+            else
+            {
+                imageElement.SafeMergeAttribute("src", urlHelper.Kentico().ImageUrl(imageUrl, SizeConstraint.Empty));
+            }
+
+            TagBuilder containerElement = new TagBuilder("div");
+            containerElement.AddCssClass("js-focuspoint");
+            containerElement.AddCssClass(cssContainerClass);
+
+            containerElement.SafeMergeAttribute("data-focus-x", focusX);
+            containerElement.SafeMergeAttribute("data-focus-y", focusY);
+            containerElement.InnerHtml += imageElement.ToString(TagRenderMode.SelfClosing);
+
+            return MvcHtmlString.Create(containerElement.ToString(TagRenderMode.Normal));
+        }
+
+        private static void SafeMergeAttribute(this TagBuilder tagElement, string attrName, string attrValue)
+        {
+            if (!String.IsNullOrEmpty(attrValue))
+            {
+                tagElement.MergeAttribute(attrName, attrValue);
+            }
+        }
+
+        private static void AddSafe(this IDictionary<string, object> collection, string key, object value)
+        {
+            if (collection.ContainsKey(key))
+            {
+                collection[key] = value;
+            }
+            else
+            {
+                collection.Add(key, value);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# kentico-mvcformcontrol-focuspoint
+# Kentico MVC Focuspoint tool
+[![MIT](https://camo.githubusercontent.com/52ec9e2dfec7264e254fb7af5ac87f301ced9180/68747470733a2f2f696d672e736869656c64732e696f2f707970692f6c2f417270656767696f2e737667)](https://raw.githubusercontent.com/hyperium/hyper/master/LICENSE)
+
 Focuspoint provides helper tool for Kentico CMS and handle image presentation, by ensuring the 'spare' parts of your image (negative space) are cropped out before the important parts.
+
+## Requirements
+* **Kentico 12.0.77** or later version is required to use this component.
+
+## Download & instalation
+1. Download and install form control package:
+    * Download and install Kentico export package from this [link](https://github.com/drilic/kentico-mvcformcontrol-focuspoint/blob/master/EXLRT.Kentico.CMS.FormControls.Focuspoint.1.0.0.zip).
+      * Import package **EXLRT.Kentico.CMS.FormControls.Focuspoint.1.0.0.zip** through Kentico Admin Portal.
+      * Make sure that checkbox 'Import code files' is checked.
+      * After import, navigate through the newly imported files and include them into project:
+        * \CMSModules\Content\FormControls\EXLRT\ImageUrlFocusPoint
+        * \CMSScripts\EXLRT\ImageUrlFocusPointSelector
+    * Download and instal MVC site extension:
+      * Download nuget package from this [link](https://github.com/drilic/kentico-mvcformcontrol-focuspoint/blob/master/EXLRT.Kentico.Mvc.FormControls.Focuspoint.1.0.0.nupkg).
+      * Setup local nugetfeed and copy nuget package **EXLRT.Kentico.Mvc.FormControls.Focuspoint.1.0.0.nupkg** to newly created feed.
+      * Install package to your MVC solution.
+2. [Assign 'Image with focuspoint' form control to text field on page type](#using-focuspoint-component)
+3. [Register necessary styles and scripts for full calendar in layout](#register-layout-scripts)
+4. [Prerequisites for HTML styles](#prerequisites-html-styles)
+5. [Render image with focuspoint extension](#focuspoint-extension)
+
+### Using focuspoint component
+
+After configuring page type field to use 'Image with focuspoint' form control and selecting image, control will render the image bellow which will provide you some kind of the preview where you will be able to select the focus point by clicking on the image itself.
+
+![Image of focuspoint control](https://github.com/drilic/kentico-mvcformcontrol-focuspoint/blob/master/focuspoint-helper-tool.png)
+
+### Prerequisites HTML styles
+
+Focus point prerequisite is to already have defined containers in which image will be cropped. That is pretty much common flow in the design which will prevent content jumping around after image is loaded:
+```html
+<style>
+	// sets the height of the image container
+        .focuspoint-testing {
+            height: 380px;
+        }
+
+	// hide image inside container to prevent image to be loaded before calculation was finished
+        .focuspoint-testing img {
+            display: none;
+        }
+</style>
+```
+
+
+
+### Register layout scripts
+
+Example of configuration for DancingGoat site (_Layout.cshtml):
+```csharp
+@using EXLRT.Kentico.Mvc.FormControls.Focuspoint.Extensions;
+...
+<head>
+	...
+	 @Html.RenderFocuspointStyles()
+</head>
+<body>
+	...
+	@Html.RenderFocuspointSripts();
+</body>
+
+```
+
+### Focuspoint extension
+
+After all is done, you can just pass the value that you get from the CMS side for the image into the FocuspointImage extension:
+```csharp
+@Html.FocuspointImage(Model.BannerImageUrl, "focuspoint-testing")
+```
+Extension above will render the HTML with all necessary parameters for focuspoint to work properly. Also, there is a few extra parameters which will allow you to customize the classes, alt and title text and much more.
+```html
+<div class="focuspoint-testing js-focuspoint" data-focus-x="0.640859375" data-focus-y="-0.2651646447140381">
+     <img src="/DancingGoatMvc/media/CoffeeGallery/hompage-turkish-airlines-case-study.jpg" style="top: -209.474%; left: 0px; display: inline;">
+</div>
+```
+
+## Contributions and Support
+Feel free to fork and submit pull requests or report issues to contribute. Either this way or another one, we will look into them as soon as possible. 
